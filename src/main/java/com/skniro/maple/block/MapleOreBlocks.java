14d889a8406d055ef22488b4f72b6698227c55d1
skniro <46392259+skniro@users.noreply.github.com>
2023-08-15 16:31:00 +0800

    Fix Block register.

--- src/main/java/com/skniro/maple/block/MapleOreBlocks.java
+++ src/main/java/com/skniro/maple/block/MapleOreBlocks.java
@@ -6,24 +6,25 @@ import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
-import net.minecraft.util.registry.Registry;
 
 public class MapleOreBlocks {
-    public static final Block Salt_Ore =registerBlock("salt_ore",new OreBlock(AbstractBlock.Settings.of(Material.STONE).requiresTool().strength(3.0F, 3.0F), UniformIntProvider.create(3, 7)),Maple.Maple_Group);
-    public static final Block DEEPSLATE_Salt_Ore =registerBlock("deepslate_salt_ore",new OreBlock(AbstractBlock.Settings.copy(Salt_Ore).mapColor(MapColor.DEEPSLATE_GRAY).strength(4.5F, 3.0F).sounds(BlockSoundGroup.DEEPSLATE), UniformIntProvider.create(3, 7)),Maple.Maple_Group);
+    public static final Block Salt_Ore =registerBlock("salt_ore",new ExperienceDroppingBlock(AbstractBlock.Settings.of(Material.STONE).requiresTool().strength(3.0F, 3.0F), UniformIntProvider.create(3, 7)),Maple.Maple_Group);
+    public static final Block DEEPSLATE_Salt_Ore =registerBlock("deepslate_salt_ore",new ExperienceDroppingBlock(AbstractBlock.Settings.copy(Salt_Ore).mapColor(MapColor.DEEPSLATE_GRAY).strength(4.5F, 3.0F).sounds(BlockSoundGroup.DEEPSLATE), UniformIntProvider.create(3, 7)),Maple.Maple_Group);
 
     private static Block registerBlockWithoutItem(String name, Block block) {
-        return Registry.register(Registry.BLOCK, new Identifier(Maple.MOD_ID, name), block);
+        return Registry.register(Registries.BLOCK, new Identifier(Maple.MOD_ID, name), block);
     }
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
-        return Registry.register(Registry.BLOCK, new Identifier(Maple.MOD_ID, name), block);
+        return Registry.register(Registries.BLOCK, new Identifier(Maple.MOD_ID, name), block);
     }
     private static Item registerBlockItem(String name, Block block, ItemGroup tab) {
-        return Registry.register(Registry.ITEM, new Identifier(Maple.MOD_ID, name),
-                new BlockItem(block, new FabricItemSettings().group(tab)));
+        return Registry.register(Registries.ITEM, new Identifier(Maple.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
     }
 }
--- src/main/java/com/skniro/maple/block/MapleSignBlocks.java
+++ src/main/java/com/skniro/maple/block/MapleSignBlocks.java
@@ -4,8 +4,9 @@ import com.skniro.maple.Maple;
 import com.skniro.maple.block.entity.MapleSignTypes;
 import net.minecraft.block.*;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class MapleSignBlocks {
     public static final Block CHERRY_SIGN = registerBlockWithoutItem("cherry_sign", new SignBlock(AbstractBlock.Settings.of(Material.WOOD, MapleBlocks.CHERRY_PLANKS.getDefaultMapColor()).noCollision().strength(1.0F), MapleSignTypes.CHERRY),Maple.Maple_Group);
@@ -16,7 +17,7 @@ public class MapleSignBlocks {
     public static final Block BAMBOO_WALL_SIGN = registerBlockWithoutItem("bamboo_wall_sign",new WallSignBlock(AbstractBlock.Settings.of(Material.WOOD, MapleBlocks.BAMBOO_PLANKS.getDefaultMapColor()).noCollision().strength(1.0F).dropsLike(BAMBOO_SIGN),MapleSignTypes.BAMBOO),Maple.Maple_Group);
 
     private static Block registerBlockWithoutItem(String name, Block block, ItemGroup group) {
-        return Registry.register(Registry.BLOCK, new Identifier(Maple.MOD_ID, name), block);
+        return Registry.register(Registries.BLOCK, new Identifier(Maple.MOD_ID, name), block);
     }
 
     public static void registerMapleSignBlocks() {
