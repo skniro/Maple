ec745f474784dbb6379ded42d7f2729c15928c82
skniro <46392259+skniro@users.noreply.github.com>
2025-01-23 16:58:16 +0800

    1.3.4 Fix forge and neoforge server crash. #35

--- Neoforge/src/main/java/com/skniro/maple/Maple.java
+++ Neoforge/src/main/java/com/skniro/maple/Maple.java
@@ -95,7 +95,7 @@ public class Maple{
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD)
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
 
         @SubscribeEvent
@@ -111,7 +111,7 @@ public class Maple{
         }
     }
 
-    @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ParticleFactoryRegistry {
         @SubscribeEvent
         public static void onParticleFactoryRegistration(RegisterParticleProvidersEvent event) {
