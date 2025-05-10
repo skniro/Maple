6cc6269d5920e5b8350b7c2083f6776f7c121721
skniro <46392259+skniro@users.noreply.github.com>
2025-05-10 17:40:29 +0800

    Fixed the issue where the game crashed when some snowballs hit the boat.

--- src/main/java/com/skniro/maple/entity/projectile/thrown/MaplePoisonSnowballEntity.java
+++ src/main/java/com/skniro/maple/entity/projectile/thrown/MaplePoisonSnowballEntity.java
@@ -6,7 +6,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.BlazeEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 
@@ -24,7 +23,9 @@ public class MaplePoisonSnowballEntity extends MapleSnowballEntity {
         Entity entity = entityHitResult.getEntity();
         int i = entity instanceof BlazeEntity ? 4 : 0;
         entity.damage(this.getDamageSources().thrown(this, this.getOwner()), i);
-        LivingEntity livingEntity = (LivingEntity) entityHitResult.getEntity();;
-        livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON,30,1));
+        if(entity instanceof LivingEntity) {
+            LivingEntity playerEntity = (LivingEntity) entityHitResult.getEntity();
+            playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 30, 1));
+        }
     }
 }
