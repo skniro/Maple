a517d22f5dbd2cd90b55c7db9f6ac229e0e16391
skniro <46392259+skniro@users.noreply.github.com>
2025-06-07 21:29:30 +0800

    Fix the stacked bottled milk will be consumed in one time.

--- src/main/java/com/skniro/maple/item/init/food/ItemBottle.java
+++ src/main/java/com/skniro/maple/item/init/food/ItemBottle.java
@@ -1,11 +1,14 @@
 package com.skniro.maple.item.init.food;
 
 import com.skniro.maple.item.MapleFoodComponents;
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
@@ -17,13 +20,28 @@ public class ItemBottle
         super(settings);
     }
 
-
+    @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-        if (!world.isClient && stack.isOf(MapleFoodComponents.MILK_BOTTOM)) {
-            user.clearStatusEffects();
+        PlayerEntity playerEntity;
+        super.finishUsing(stack, world, user);
+
+        // Advancement check + stat
+        if (user instanceof ServerPlayerEntity serverPlayerEntity) {
+            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
         }
-        ItemStack itemStack = super.finishUsing(stack, world, user);
-        return user instanceof PlayerEntity && ((PlayerEntity)user).getAbilities().creativeMode ? itemStack : new ItemStack(Items.GLASS_BOTTLE);
+
+        // Clear statuses (if milk)
+        if (!world.isClient && stack.isOf(MapleFoodComponents.MILK_BOTTOM))
+            user.clearStatusEffects();
+
+        if (user instanceof PlayerEntity && !(playerEntity = (PlayerEntity) user).getAbilities().creativeMode) {
+            ItemStack itemStack = new ItemStack(Items.GLASS_BOTTLE);
+            if (!playerEntity.getInventory().insertStack(itemStack)) {
+                playerEntity.dropItem(itemStack, false);
+            }
+        }
+        return stack;
     }
 
     @Override
