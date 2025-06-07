8bffe483551070068c528ba6b408e45cfe99bb66
skniro <46392259+skniro@users.noreply.github.com>
2025-06-07 21:30:14 +0800

    Fix the stacked bottled milk will be consumed in one time.

--- src/main/java/com/skniro/maple/item/init/food/ItemBottle.java
+++ src/main/java/com/skniro/maple/item/init/food/ItemBottle.java
@@ -1,6 +1,9 @@
 package com.skniro.maple.item.init.food;
 
 import com.skniro.maple.item.MapleFoodComponents;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -17,13 +20,29 @@ public class ItemBottle
         super(settings);
     }
 
-
+    @Override
     public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity user) {
-        if (!world.isClientSide && stack.is(MapleFoodComponents.MILK_BOTTOM.get())) {
-            user.removeAllEffects();
+        Player playerEntity;
+        super.finishUsingItem(stack, world, user);
+
+        // Advancement check + stat
+        if (user instanceof ServerPlayer serverPlayerEntity) {
+            CriteriaTriggers.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.awardStat(Stats.ITEM_USED.get(this));
         }
-        ItemStack itemStack = super.finishUsingItem(stack, world, user);
-        return user instanceof Player && ((Player)user).getAbilities().instabuild ? itemStack : new ItemStack(Items.GLASS_BOTTLE);
+
+        // Clear statuses (if milk)
+        if (!world.isClientSide && stack.is(MapleFoodComponents.MILK_BOTTOM.get()))
+            user.removeAllEffects();
+
+
+        if (user instanceof Player && !(playerEntity = (Player)user).getAbilities().instabuild) {
+            ItemStack itemStack = new ItemStack(Items.GLASS_BOTTLE);
+            if (!playerEntity.getInventory().add(itemStack)) {
+                playerEntity.drop(itemStack, false);
+            }
+        }
+        return stack;
     }
 
     @Override
